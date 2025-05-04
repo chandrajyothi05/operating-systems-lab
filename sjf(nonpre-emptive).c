3d8368833ffff4bb7cc6e371c09442e7063d281c
Chandrajyothi Parambi Biju <153224157+chandrajyothi05@users.noreply.github.com>
2025-05-04 15:48:17 +0530

    Update sjf(nonpre-emptive).c

--- sjf(nonpre-emptive).c
+++ sjf(nonpre-emptive).c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int n;
-float totaltat=0,totalwt=0;
+float totaltat = 0,totalwt = 0;
 struct Process {
     int pid;
     int at;
@@ -23,31 +23,31 @@ void sortByArrival() {
     }
 }
 void calct(){
-    p[0].ct=p[0].at+p[0].bt;
-    for(int i=1;i<n;i++){
-        if(p[i].at>p[i-1].ct){
-            p[i].ct=p[i].at+p[i].bt;
+    p[0].ct = p[0].at + p[0].bt;
+    for(int i=1; i<n; i++){
+        if(p[i].at > p[i-1].ct){
+            p[i].ct = p[i].at + p[i].bt;
         }
         else{
-            p[i].ct=p[i-1].ct+p[i].bt;
+            p[i].ct = p[i-1].ct + p[i].bt;
         }
     }
 }
 void calttwt(){
-    for(int i=0;i<n;i++){
-        p[i].tat=p[i].ct-p[i].at;
-        totaltat+=p[i].tat;
-        p[i].wt=p[i].tat-p[i].bt;
-        totalwt+=p[i].wt;
+    for(int i=0; i<n; i++){
+        p[i].tat = p[i].ct - p[i].at;
+        totaltat += p[i].tat;
+        p[i].wt = p[i].tat - p[i].bt;
+        totalwt += p[i].wt;
     }
 }
 int main() {
     printf("enter no. of processes\n");
     scanf("%d",&n);
     printf("enter arrival time and burst time of %d processes\n",n);
-    for(int i=0;i<n;i++)
+    for(int i=0; i<n; i++)
     {
-        p[i].pid=i+1;
+        p[i].pid = i + 1;
         scanf("%d%d",&p[i].at,&p[i].bt);
     }
     sortByArrival();
